8253726c57dd06ae037d9f0aa56b8965f8ceb3b2
Eyad Ali <51314695+eyadali05@users.noreply.github.com>
2022-11-01 22:51:41 +0800

    Version 1.0
    - Fully Working GUI with Logic
    - Provided both CLI and GUI versions
    - Added Receipt Snapshot Feature
    - Packaged GUI

--- tnb_get_bill.m
+++ tnb_get_bill.m
@@ -1,17 +1,10 @@
-function [bill] = tnb_get_bill(broken_power)
+function [total_consumption_money, icpt, st, final_bill] = tnb_get_bill(broken_power)
 %TNB_GET_BILL:calculates estimated bill (with ICPT and ST) given power list
 broken_power_len = length(broken_power);
 disp(broken_power)
 current_rates = [0.2180, 0.3340, 0.5160, 0.5460, 0.5710]
 consumed_power = sum(broken_power)
 icpt = consumed_power * -0.02
-
-if consumed_power > 600
-    money_tax = 0.526 * (consumed_power - 600);
-    st = 0.06 * money_tax
-else
-    st = 0
-end
 
 if broken_power_len == 0
     final_bill = 3
@@ -20,7 +13,22 @@ else
     for p=1:broken_power_len
         estimated_bill_list = [estimated_bill_list, broken_power(p) * current_rates(p)];
     end
-    final_bill = sum(estimated_bill_list) + icpt + st
+    total_consumption_money = sum(estimated_bill_list);
+    if consumed_power > 600
+        st_applicable_list = [];
+        for e=4:broken_power_len
+            st_applicable_list = [st_applicable_list, estimated_bill_list(e)]
+        end
+        st_applicable_consumption = sum(st_applicable_list)
+        st_applicable_consumption_post_icpt = st_applicable_consumption + (icpt + 12)
+        st = 0.06 * st_applicable_consumption_post_icpt
+        st_not_applicable_post_icpt = sum(estimated_bill_list(1:3)) - 12
+        final_bill = st_not_applicable_post_icpt + st_applicable_consumption_post_icpt + st
+    else
+        st = 0
+        final_bill= total_consumption_money + icpt + st
+    end
+       
 end
 
 end
